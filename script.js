2a0df9ee7c45320c0a495bf673feed21b3a49c26
Shubham-547 <personal.shubham1872@gmail.com>
2026-06-05 17:35:08 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+let Name= prompt(`What is your name?`);
+
+if (Name == `Shubham`){
+    alert(`Welcome Back SHUBHAM`);
+}
+else if (Name == `shubham`){
+    alert(`Welcome Back SHUBHAM`);
+}
+else if (Name == `SHUBHAM`){
+    alert(`Welcome Back SHUBHAM`);
+}
+else{
+    alert(`you are not SHUBHAM.
+                WHO ARE YOU.`);
+}
